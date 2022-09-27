a0fb2bdc1c39372a8d07a2b7259e7bc666d60220
Victor Matias <fvictor1705@gmail.com>
2022-09-27 15:07:09 -0300

    Resolve requisito 21.

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -5,13 +5,20 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import CardRecipe from '../components/CardRecipe';
 import fetchApi from '../services/fetchApi';
+import LoadingComponent from '../components/LoadingComponent';
 
 const FOODS_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 const DRINKS_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const LIST_RECIPE_FOODS = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 const LIST_RECIPE_DRINKS = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+const URL_FILTER_FOODS = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=';
+const URL_FILTER_DRINKS = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=';
+const ONZE = 11;
+let currKey = '';
+let URL = '';
 
 export default function Recipes() {
+  const [loading, setLoading] = useState(true);
   const [recipesState, setRecipesState] = useState({
     recipes: [],
     type: '',
@@ -25,10 +32,9 @@ export default function Recipes() {
   const { location: { pathname } } = useHistory();
 
   useEffect(() => {
-    let URL = '';
+    setLoading(true);
+
     let URL_LIST = '';
-    let currKey = '';
-    const ONZE = 11;
     const QUATRO = 4;
 
     switch (pathname) {
@@ -52,6 +58,7 @@ export default function Recipes() {
         recipes: onlyTwelveFirst,
         type: currKey,
       });
+      setLoading(false);
     };
     const fetchRecipeList = async () => {
       const request = await fetchApi(URL_LIST);
@@ -66,32 +73,76 @@ export default function Recipes() {
     fetchRecipeList();
   }, [pathname]); // eslint-disable-line
 
+  const getByCategory = async (category) => {
+    setLoading(true);
+    let URL_CATEGORY = '';
+    switch (pathname) {
+    case '/drinks': {
+      URL_CATEGORY = URL_FILTER_DRINKS + category;
+      break;
+    }
+    default: {
+      URL_CATEGORY = URL_FILTER_FOODS + category;
+    }
+    }
+
+    const data = await fetchApi(URL_CATEGORY);
+    const onlyTwelveFirst = data[currKey]?.filter((e, i) => i <= ONZE);
+    setRecipesState({ ...recipesState, recipes: onlyTwelveFirst });
+    setLoading(false);
+  };
+
+  const clearFilters = async () => {
+    setLoading(true);
+
+    const request = await fetchApi(URL);
+    const onlyTwelveFirst = request[currKey].filter((e, i) => i <= ONZE);
+    setRecipesState({
+      recipes: onlyTwelveFirst,
+      type: currKey,
+    });
+
+    setLoading(false);
+  };
+
   return (
-    <div>
+    <main>
       <Header />
       <ButtonSearch />
-      <section>
-        { recipeList.list.length > 0 ? recipeList.list.map(({ strCategory }) => (
-          <button
-            key={ strCategory }
-            type="button"
-            data-testid={ `${strCategory}-category-filter` }
-          >
-            {strCategory}
-          </button>
-        )) : null }
-      </section>
-      <section>
-        { recipesState.recipes.length > 0 ? recipesState.recipes.map((recipe, i) => (
-          <CardRecipe
-            key={ i }
-            type={ recipesState.type }
-            recipe={ recipe }
-            index={ i }
-          />
-        )) : <p>Carregando...</p> }
-      </section>
+      { loading ? <LoadingComponent /> : (
+        <section>
+          <section>
+            { recipeList.list.length > 0 ? recipeList.list.map(({ strCategory }) => (
+              <button
+                key={ strCategory }
+                type="button"
+                onClick={ () => getByCategory(strCategory) }
+                data-testid={ `${strCategory}-category-filter` }
+              >
+                {strCategory}
+              </button>
+            )) : null }
+            <button
+              type="button"
+              onClick={ clearFilters }
+              data-testid="All-category-filter"
+            >
+              All
+            </button>
+          </section>
+          <section>
+            { recipesState.recipes?.length > 0 ? recipesState.recipes.map((recipe, i) => (
+              <CardRecipe
+                key={ i }
+                type={ recipesState.type }
+                recipe={ recipe }
+                index={ i }
+              />
+            )) : <span>Sem receitas para essa categoria</span> }
+          </section>
+        </section>
+      ) }
       <Footer />
-    </div>
+    </main>
   );
 }
