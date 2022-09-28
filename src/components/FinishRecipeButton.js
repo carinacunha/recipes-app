8afb85a40bd75787bcf2b61a09af4d088c43d728
vitorlichoti <vitorlichoti@gmail.com>
2022-09-28 14:41:29 -0400

    Requisitos 37, 38 e 39

---
+++ src/components/FinishRecipeButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FinishRecipeButton() {
+  return (
+    <div>
+      <button type="button" data-testid="finish-recipe-btn">Finish Recipe</button>
+    </div>
+  );
+}
+
+export default FinishRecipeButton;
