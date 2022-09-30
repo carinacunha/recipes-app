900f79c9a708ab1ad5b08aa591ea186435356e9e
vitorlichoti <vitorlichoti@gmail.com>
2022-09-30 16:20:34 -0400

    Finaliza página Favorite Recipes

---
+++ src/components/FavoriteInputs.js
@@ -0,0 +1,82 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ShareButtonDone from './ShareButtonDone';
+
+function CardDoneRecipe({ recipe, index }) {
+  const { type, id } = recipe;
+  const url = type === 'meal' ? `/meals/${id}`
+    : `/drinks/${id}`;
+
+  return (
+
+    <div>
+      <Link to={ url }>
+        <img
+          width="270px"
+          data-testid={ `${index}-horizontal-image` }
+          src={ recipe.image }
+          alt="imagem not found"
+        />
+      </Link>
+      {
+        recipe.type === 'meal' ? (
+          <h3
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            { `${recipe.nationality} - ${recipe.category}` }
+          </h3>
+        ) : (
+          <h3
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            { recipe.alcoholicOrNot }
+          </h3>
+        )
+      }
+      <Link to={ url }>
+        <h3
+          data-testid={ `${index}-horizontal-name` }
+        >
+          { recipe.name }
+        </h3>
+      </Link>
+
+      <h3
+        data-testid={ `${index}-horizontal-done-date` }
+      >
+        { recipe.doneDate }
+      </h3>
+
+      <ShareButtonDone recipe={ recipe } index={ index } />
+
+      <section>
+        {
+          recipe.tags.map((tag) => (
+            <p
+              key={ tag }
+              data-testid={ `${index}-${tag}-horizontal-tag` }
+            >
+              { tag }
+            </p>
+          ))
+        }
+      </section>
+
+    </div>
+
+  );
+}
+
+CardDoneRecipe.propTypes = {
+  index: PropTypes.number,
+  recipe: PropTypes.shape({
+    category: PropTypes.string,
+    doneDate: PropTypes.string,
+    image: PropTypes.string,
+    name: PropTypes.string,
+    tags: PropTypes.shape(),
+  }),
+}.isRequired;
+
+export default CardDoneRecipe;
--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,47 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
-import CardDoneRecipe from '../components/CardDoneRecipe';
+import CardFavoriteRecipe from '../components/CardFavoriteRecipe';
 
-const done = [
-  {
-    id: '52771',
-    type: 'meal',
-    nationality: 'Italian',
-    category: 'Vegetarian',
-    alcoholicOrNot: '',
-    name: 'Spicy Arrabiata Penne',
-    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    doneDate: '23/06/2020',
-    tags: ['Pasta', 'Curry'],
-  },
-  {
-    id: '178319',
-    type: 'drink',
-    nationality: '',
-    category: 'Cocktail',
-    alcoholicOrNot: 'Alcoholic',
-    name: 'Aquamarine',
-    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    doneDate: '23/06/2020',
-    tags: [],
-  },
-];
+function FavoriteRecipes(props) {
+  const [favoriteRecipes, setFavoriteRecipes] = useState();
+  const { history } = props;
 
-function FavoriteRecipes() {
-  const [doneRecipes, setDoneRecipes] = useState(done);
+  useEffect(() => {
+    const getFavoriteStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    setFavoriteRecipes(getFavoriteStorage);
+  }, []);
 
   const filterByType = ({ target }) => {
     if (target.name === 'meal') {
-      const filterMeals = done.filter((elem) => elem.type === target.name);
-      setDoneRecipes(filterMeals);
-      return;
+      const localFavorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      const filterMeals = localFavorites.filter((elem) => elem.type === target.name);
+      setFavoriteRecipes(filterMeals);
+    } else if (target.name === 'drink') {
+      const localFavorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      const filterDrinks = localFavorites.filter((elem) => elem.type === target.name);
+      setFavoriteRecipes(filterDrinks);
+    } else if (target.name === 'all') {
+      const localFavorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      setFavoriteRecipes(localFavorites);
     }
-    if (target.name === 'drink') {
-      const filterDrinks = done.filter((elem) => elem.type === target.name);
-      setDoneRecipes(filterDrinks);
-      return;
-    } setDoneRecipes(done);
   };
+
   return (
     <div>
       <Header />
@@ -72,12 +57,31 @@ function FavoriteRecipes() {
         Drinks
       </button>
 
+      <button
+        name="back"
+        type="button"
+        onClick={ () => history.push('/meals') }
+      >
+        Go Home
+      </button>
+
       {
-        doneRecipes.map((recipe, index) => (
-          <CardDoneRecipe recipe={ recipe } key={ index } index={ index } />))
+        favoriteRecipes?.map((recipe, index) => (
+          <CardFavoriteRecipe
+            recipe={ recipe }
+            key={ index }
+            index={ index }
+            setFavoriteRecipes={ setFavoriteRecipes }
+          />))
       }
     </div>
   );
 }
+
+FavoriteRecipes.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default FavoriteRecipes;
